734a2d821946618643787d3221f2aabe8702ad80
Mankirat Singh <mankirat1998@gmail.com>
2018-04-01 16:49:05 +0530

    feat: initializing folder structure

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express= require("express"),
+    app= express(),
+    cors= require("cors"),
+    bodyParser= require("body-parser");
+
+const PORT= 8081;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// all routes
+
+app.use(function (req,res,next) {
+    let err=new Error("Not Found");
+    err.status= 404;
+    next(err);
+});
+
+app.listen(PORT, function () {
+    console.log(`Server starting on PORT ${PORT}`);
+});
