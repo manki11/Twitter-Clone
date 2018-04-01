452edfba16a887fbe44cf13759af250a07ccf306
Mankirat Singh <mankirat1998@gmail.com>
2018-04-01 18:05:51 +0530

    feat: user schema password handling added

--- server/models/user.js
+++ server/models/user.js
@@ -21,6 +21,30 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
+UserSchema.pre("save", async function (next) {
+    try{
+        if(!this.isModified("password")){
+            return next();
+        }
+        let hashedPassword= await bcrypt.hash(this.password, 10);
+        this.password= hashedPassword;
+        return next();
+    }
+    catch(err){
+        return next(err);
+    }
+});
+
+UserSchema.methods.comparePassword= async function(candidatePassword, next) {
+    try{
+        let isMatch= await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }
+    catch(err){
+        next(err);
+    }
+}
+
 const User= mongoose.model("User", UserSchema);
 
 module.exports= User;
