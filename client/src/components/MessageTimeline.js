41862209b14681a8da03226b4e7ffd399619aa99
Mankirat Singh <mankirat1998@gmail.com>
2018-04-06 00:04:08 +0530

    feat: timeline component added

---
+++ client/src/components/MessageTimeline.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MessageList from "../containers/MessageList";
+
+const MessageTimeline= props => {
+    return(
+        <div className="row">
+            <MessageList/>
+        </div>
+    )
+};
+
+export default MessageTimeline;
